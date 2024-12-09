684c3224525efa32d3c4a6da72932f68f64ebb14
Vitor Fel <vitorfel2000@gmail.com>
2024-12-09 19:51:22 -0300

    test(AppComponent): Cria teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,47 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { HeaderComponent } from 'src/shared/components/header/header.component';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  template: '',
+})
+class FakeHeaderComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule],
+      imports: [AppComponent],
     }).compileComponents();
+
+    TestBed.overrideComponent(AppComponent, {
+      remove: {
+        imports: [HeaderComponent],
+      },
+      add: {
+        imports: [FakeHeaderComponent],
+      },
+    });
   });
 
-  it('should render title', () => {
+  it('Deve renderizar o componente header', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
+    const headerDebugEl = fixture.debugElement.query(By.css('app-header'));
+
+    expect(headerDebugEl).toBeTruthy();
+  });
+
+  it('Deve renderizar o componente router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const routerOutletDebugEl = fixture.debugElement.query(
+      By.css('router-outlet')
+    );
+
+    expect(routerOutletDebugEl).toBeTruthy();
   });
 });
