0f8bd818dd54aef44eb3a6b46e9f817d90cc86b0
Vitor Fel <vitorfel2000@gmail.com>
2024-12-18 19:43:51 -0300

    feat(TasksService): Utiliza HttpClient para fazer requisições

--- src/app/shared/services/tasks/tasks.service.ts
+++ src/app/shared/services/tasks/tasks.service.ts
@@ -1,19 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Task } from '../../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
+  httpClient = inject(HttpClient);
+
   getAll(): Observable<Task[]> {
-    return of([
-      { title: 'Item 1', completed: false },
-      { title: 'Item 2', completed: false },
-      { title: 'Item 3', completed: false },
-      { title: 'Item 4', completed: true },
-      { title: 'Item 5', completed: true },
-      { title: 'Item 6', completed: true },
-    ]);
+    return this.httpClient.get<Task[]>('/tasks');
   }
 }
