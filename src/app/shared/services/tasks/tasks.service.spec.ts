5223cbf1f168ff2ca17c03760a3e22640db419c7
Vitor Fel <vitorfel2000@gmail.com>
2025-03-18 20:10:39 -0300

    test(TasksService): Testa método patch

--- src/app/shared/services/tasks/tasks.service.spec.ts
+++ src/app/shared/services/tasks/tasks.service.spec.ts
@@ -31,12 +31,12 @@ describe('TasksService', () => {
     const request = httpTestingController.expectOne('/api/tasks');
 
     const fakeTasks: Task[] = [
-      { title: 'Item 1', completed: false },
-      { title: 'Item 2', completed: false },
-      { title: 'Item 3', completed: false },
-      { title: 'Item 4', completed: true },
-      { title: 'Item 5', completed: true },
-      { title: 'Item 6', completed: true },
+      { id: '1', title: 'Item 1', completed: false },
+      { id: '2', title: 'Item 2', completed: false },
+      { id: '3', title: 'Item 3', completed: false },
+      { id: '4', title: 'Item 4', completed: true },
+      { id: '5', title: 'Item 5', completed: true },
+      { id: '6', title: 'Item 6', completed: true },
     ];
 
     request.flush(fakeTasks);
@@ -44,5 +44,26 @@ describe('TasksService', () => {
     tick();
 
     expect(result).toEqual(fakeTasks);
+  }));
+
+  it('patch() deve atualizar uma tarefa', fakeAsync(() => {
+    const fakeTask: Task = { id: '1', title: 'Item 1', completed: false };
+    let result: Task | null = null;
+
+    service.patch(fakeTask.id, { completed: true }).subscribe((response) => {
+      result = response;
+    });
+
+    const request = httpTestingController.expectOne((req) => {
+      return req.method === 'PATCH' && req.url === '/api/tasks/1';
+    });
+
+    const fakeResponse = { ...fakeTask, completed: true };
+
+    request.flush(fakeResponse);
+
+    tick();
+
+    expect(result).toEqual(fakeResponse);
   }));
 });
