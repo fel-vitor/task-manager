eb77dbe21d9f975d77b8c814d02cd2ee8faab9f8
Vitor Fel <vitorfel2000@gmail.com>
2025-03-24 19:28:40 -0300

    test(AppComponent): Usa ng-mocks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { MockComponent } from 'ng-mocks';
 import { HeaderComponent } from 'src/shared/components/header/header.component';
 import { AppComponent } from './app.component';
-import { FakeHeaderComponent } from '@testing/mocks/fake-header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
         imports: [HeaderComponent],
       },
       add: {
-        imports: [FakeHeaderComponent],
+        imports: [MockComponent(HeaderComponent)],
       },
     });
   });
