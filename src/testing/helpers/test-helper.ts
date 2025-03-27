aada07e2c053a1f3a5b44b03c0b823c4e4822b27
Vitor Fel <vitorfel2000@gmail.com>
2025-03-27 18:38:58 -0300

    test(TestHelper): Adiciona métodos utilitários

--- src/testing/helpers/test-helper.ts
+++ src/testing/helpers/test-helper.ts
@@ -12,6 +12,32 @@ export class TestHelper<T> {
     this.queryByTestId(testId).nativeElement.click();
   }
 
+  getInputValue(testId: string) {
+    return this.queryByTestId(testId).nativeElement.value;
+  }
+
+  isCheckboxChecked(testId: string) {
+    return this.queryByTestId(testId).nativeElement.checked;
+  }
+
+  triggerInputEvent(testId: string, value: string) {
+    this.queryByTestId(testId).triggerEventHandler('input', {
+      target: {
+        value,
+      },
+    });
+  }
+
+  changeCheckbox(testId: string, value: boolean) {
+    this.queryByTestId(testId).triggerEventHandler('change', {
+      target: { checked: value },
+    });
+  }
+
+  submitForm(testId: string) {
+    this.queryByTestId(testId).triggerEventHandler('submit', null);
+  }
+
   queryByTestId(testId: string) {
     return this.fixture.debugElement.query(By.css(`[data-testid="${testId}"]`));
   }
