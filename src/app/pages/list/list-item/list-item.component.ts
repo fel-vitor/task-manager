6c4363fe961a4d2bfe1ccffe6139c588eca26a2e
Vitor Fel <vitorfel2000@gmail.com>
2025-04-02 18:57:37 -0300

    feat(ListItemComponent): Usa CapitalizePipe

--- src/app/pages/list/list-item/list-item.component.ts
+++ src/app/pages/list/list-item/list-item.component.ts
@@ -2,11 +2,12 @@ import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from 'src/app/shared/interfaces/task.interface';
 import { ButtonXsDirective } from 'src/app/shared/directives/button/button.directive';
+import { CapitalizePipe } from 'src/app/shared/pipes/capitalize/capitalize.pipe';
 
 @Component({
   selector: 'app-list-item',
   standalone: true,
-  imports: [CommonModule, ButtonXsDirective],
+  imports: [CommonModule, ButtonXsDirective, CapitalizePipe],
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.scss',
 })
