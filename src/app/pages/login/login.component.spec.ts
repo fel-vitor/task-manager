46318beda7fff4bf65740b501e51abc8cef369f7
Vitor Fel <vitorfel2000@gmail.com>
2025-04-02 20:42:16 -0300

    test(LoginComponent): Testa renderização de mensagem de erro quando autenticação falhar e outras coisas

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,8 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { TestHelper } from '@testing/helpers/test-helper';
 import { MockProviders } from 'ng-mocks';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { LoginComponent } from './login.component';
 
@@ -27,6 +28,18 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  it('Não deve chamar ação de autenticação quando o formulário estiver inválido', () => {
+    testHelper.submitForm('login-submit');
+
+    expect(authService.login).not.toHaveBeenCalled();
+    expect(router.navigateByUrl).not.toHaveBeenCalled();
+  });
+
+  it('Os campos devem iniciar vázios', () => {
+    expect(testHelper.getInputValue('login-email')).toBe('');
+    expect(testHelper.getInputValue('login-password')).toBe('');
+  });
+
   it('Deve fazer autenticação do usuário', () => {
     const fakeEmail = 'correto@dominio.com';
     const fakePassword = '123456';
@@ -43,5 +56,29 @@ describe('LoginComponent', () => {
     expect(authService.login).toHaveBeenCalledWith(fakeEmail, fakePassword);
 
     expect(router.navigateByUrl).toHaveBeenCalledWith('/');
+  });
+
+  it('Deve monstar uma mensagem de erro quando a autenticação falhar', () => {
+    const fakeEmail = 'errado@dominio.com';
+    const fakePassword = '123';
+
+    expect(testHelper.queryByTestId('login-auth-failed')).toBeFalsy();
+
+    testHelper.triggerInputEvent('login-email', fakeEmail);
+    testHelper.triggerInputEvent('login-password', fakePassword);
+
+    (authService.login as jest.Mock).mockReturnValue(
+      throwError(() => new HttpErrorResponse({ status: 401 }))
+    );
+
+    testHelper.submitForm('login-submit');
+
+    expect(authService.login).toHaveBeenCalledWith(fakeEmail, fakePassword);
+
+    expect(router.navigateByUrl).not.toHaveBeenCalled();
+
+    fixture.detectChanges();
+
+    expect(testHelper.queryByTestId('login-auth-failed')).toBeTruthy();
   });
 });
