11762803df77c1c250f676cdffa9268df24a2815
Vitor Fel <vitorfel2000@gmail.com>
2025-04-07 18:55:20 -0300

    feat(appRoutes): Adiciona guarda isLoggedInGuard as rotas que precisam de autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,35 @@
 import { Route } from '@angular/router';
 import { getTaskByIdResolver } from './shared/resolvers/get-task-by-id/get-task-by-id.resolver';
+import { isLoggedInGuard } from './shared/guards/is-logged-in.guard';
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    loadComponent: () =>
-      import('./pages/list/list.component').then((m) => m.ListComponent),
-  },
-  {
-    path: 'create',
-    loadComponent: () =>
-      import('./pages/create-task/create-task.component').then(
-        (m) => m.CreateTaskComponent
-      ),
-  },
-  {
-    path: 'edit/:id',
-    resolve: {
-      task: getTaskByIdResolver,
-    },
-    loadComponent: () =>
-      import('./pages/edit-task/edit-task.component').then(
-        (m) => m.EditTaskComponent
-      ),
+    canActivate: [isLoggedInGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/list/list.component').then((m) => m.ListComponent),
+      },
+      {
+        path: 'create',
+        loadComponent: () =>
+          import('./pages/create-task/create-task.component').then(
+            (m) => m.CreateTaskComponent
+          ),
+      },
+      {
+        path: 'edit/:id',
+        resolve: {
+          task: getTaskByIdResolver,
+        },
+        loadComponent: () =>
+          import('./pages/edit-task/edit-task.component').then(
+            (m) => m.EditTaskComponent
+          ),
+      },
+    ],
   },
   {
     path: 'login',
