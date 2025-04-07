6dce04bbaaffb149a11b0fdc8d0029101eb64d3b
Vitor Fel <vitorfel2000@gmail.com>
2025-04-07 20:40:55 -0300

    feat(LoginFacadeService): Salva o token de autenticação em localStorage

--- src/app/shared/services/login-facade/login-facade.service.ts
+++ src/app/shared/services/login-facade/login-facade.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { of, tap } from 'rxjs';
 import { AuthStoreService } from '../../stores/auth.store';
 import { AuthService } from '../auth/auth.service';
+import { AuthTokenManagerService } from '../auth-token-manager/auth-token-manager.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,14 @@ import { AuthService } from '../auth/auth.service';
 export class LoginFacadeService {
   authStore = inject(AuthStoreService);
   authService = inject(AuthService);
+  authTokenManagerService = inject(AuthTokenManagerService);
 
   login(email: string, password: string) {
     return this.authService
       .login(email, password)
-      .pipe(tap(() => this.authStore.setAsLoggedIn()));
+      .pipe(
+        tap(() => this.authStore.setAsLoggedIn()),
+        tap(({ token }) => this.authTokenManagerService.setToken(token)),
+      );
   }
 }
