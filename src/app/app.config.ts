9cb9f3c43db2720f790c7f567c0f92fde766ecac
Vitor Fel <vitorfel2000@gmail.com>
2025-04-08 21:18:27 -0300

    feat(appConfig): Integra addAuthAuthorizationHeaderInterceptor à aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { addAuthAuthorizationHeaderInterceptor } from './core/interceptors/add-auth-authorization-header/add-auth-authorization-header.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([addAuthAuthorizationHeaderInterceptor])
+    ),
   ],
 };
