11c39561fbb605e2b4071137467fda2ba5963613
Vitor Fel <vitorfel2000@gmail.com>
2025-04-08 21:13:14 -0300

    test(AuthTokenManagerService): Testa método getToken que recupera o token

--- src/app/shared/services/auth-token-manager/auth-token-manager.service.spec.ts
+++ src/app/shared/services/auth-token-manager/auth-token-manager.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthTokenManagerService } from './auth-token-manager.service';
 import { MockProvider } from 'ng-mocks';
 import { LocalStorageToken } from '../../tokens/local-storage.token';
+import { AuthTokenManagerService } from './auth-token-manager.service';
 
 describe('AuthTokenManagerService', () => {
   let service: AuthTokenManagerService;
@@ -13,19 +13,31 @@ describe('AuthTokenManagerService', () => {
       providers: [
         MockProvider(LocalStorageToken, {
           setItem: jest.fn(),
+          getItem: jest.fn(),
         }),
-      ]
+      ],
     });
     service = TestBed.inject(AuthTokenManagerService);
     storage = TestBed.inject(LocalStorageToken);
   });
 
   it('Deve adicionar o token ao local storage', () => {
-
-    const fakeToken = 'fake-token'
+    const fakeToken = 'fake-token';
 
     service.setToken(fakeToken);
 
-    expect(storage.setItem).toHaveBeenCalledWith('auth-token', fakeToken)
+    expect(storage.setItem).toHaveBeenCalledWith('auth-token', fakeToken);
+  });
+
+  it('Deve recuperar o token do local storage', () => {
+    const fakeToken = 'fake-token';
+
+    (storage.getItem as jest.Mock).mockReturnValue(fakeToken);
+
+    const result = service.getToken();
+
+    expect(storage.getItem).toHaveBeenCalledWith('auth-token');
+
+    expect(result).toBe(fakeToken);
   });
 });
