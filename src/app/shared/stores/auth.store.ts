1602d5da1a05f5b811a392f869e94998386c43b5
Vitor Fel <vitorfel2000@gmail.com>
2025-04-14 20:50:45 -0300

    feat(AuthStoreService): Adiciona observable para notificar a mudança de estado

--- src/app/shared/stores/auth.store.ts
+++ src/app/shared/stores/auth.store.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthStoreService {
-
   #state = false;
+  #state$ = new BehaviorSubject<boolean>(false);
 
   isLoggedIn() {
     return this.#state;
   }
 
-  setAsLoggedIn() {
-    this.#state = true;
+  isLoggedIn$() {
+    return this.#state$.asObservable();
   }
 
+  setAsLoggedIn() {
+    this.#state = true;
+    this.#state$.next(this.#state);
+  }
+
+  setAsLoggedOut() {
+    this.#state = false;
+    this.#state$.next(this.#state);
+  }
 }
