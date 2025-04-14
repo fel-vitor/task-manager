1298e33922aa404eecf5aa81f32ac42562465dcd
Vitor Fel <vitorfel2000@gmail.com>
2025-04-14 20:55:52 -0300

    test(HeaderComponent): Testa integração de componente LogoutComponent

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeaderComponent } from './header.component';
 import { By } from '@angular/platform-browser';
+import { MockComponent } from 'ng-mocks';
+import { HeaderComponent } from './header.component';
+import { LogoutComponent } from './logout/logout.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -13,13 +15,26 @@ describe('HeaderComponent', () => {
 
     await TestBed.compileComponents();
 
+    TestBed.overrideComponent(HeaderComponent, {
+      remove: {
+        imports: [LogoutComponent],
+      },
+      add: {
+        imports: [MockComponent(LogoutComponent)],
+      },
+    });
+
     fixture = TestBed.createComponent(HeaderComponent);
     fixture.detectChanges();
   });
 
-  it('deve renderizar o título corretamente', () => {
+  it('Deve renderizar o título corretamente', () => {
     const h1DebugEl = fixture.debugElement.query(By.css('h1'));
 
     expect(h1DebugEl.nativeElement.textContent).toBe('Gerenciador de tarefas');
+  });
+
+  it('Deve renderizar o componente do logout', () => {
+    expect(fixture.debugElement.query(By.css('app-logout'))).toBeTruthy();
   });
 });
