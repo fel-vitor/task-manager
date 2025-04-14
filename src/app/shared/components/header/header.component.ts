c28288aff8b7972ec02ae4ceb57102f513c6ccda
Vitor Fel <vitorfel2000@gmail.com>
2025-04-14 20:55:14 -0300

    feat(HeaderComponent): Integra componente LogoutComponent

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LogoutComponent } from './logout/logout.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LogoutComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
