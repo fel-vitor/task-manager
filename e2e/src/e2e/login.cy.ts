c212a1863db3d570a5722b4b1dd97421604cc700
Vitor Fel <vitorfel2000@gmail.com>
2025-05-07 19:19:02 -0300

    test(login): [e2e] - Adiciona teste do fluxo de login

---
+++ e2e/src/e2e/login.cy.ts
@@ -0,0 +1,39 @@
+describe('Fluxo de autenticação', () => {
+  context('Quando o usuário não estiver logado', () => {
+    beforeEach(() => cy.visit('/'));
+
+    it('Deve redirecionar para rota de login, autenticar e redirecionar para a listagem', () => {
+      cy.location('pathname').should('be.eq', '/login');
+
+      cy.fixture('credentials').then((credentials) => {
+        cy.getByTestId('login-email').type(credentials.email);
+        cy.getByTestId('login-password').type(credentials.password);
+      });
+
+      cy.getByTestId('login-submit').submit();
+
+      cy.location('pathname').should('be.eq', '/');
+    });
+
+    it('Não deve existir um botão de logout', () => {
+      cy.getByTestId('header-logout').should('not.exist');
+    });
+  });
+
+  context('Quando o usuário estiver logado', () => {
+    beforeEach(() => {
+      cy.setUserAsLoggedIn();
+      cy.visit('/');
+    });
+
+    it('Deve permanecer na rota de listagem', () => {
+      cy.location('pathname').should('equal', '/');
+    });
+
+    it.only('Deve fazer o logout do usuário', () => {
+      cy.getByTestId('header-logout').click();
+
+      cy.location('pathname').should('equal', '/login');
+    });
+  });
+});
